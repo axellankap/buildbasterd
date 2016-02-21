547cacfcbb01c54a27f4457add3e3948e226dd49
Alex Kaplan <kaplan2539@gmail.com>
2016-02-21 15:07:05 -0800

    added .travis.yml and build.sh

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cat /etc/issue
+
+set -x
+
+export GIT_ORIGIN=$(git remote -v |head -n1|sed -r -e 's/.*(http(s)?:?\/\/|\w@)[A-Za-z0-9.]*[\/:]([^ ]*).*/\3/g; s/\.git//g')
+export GIT_USER=${GIT_ORIGIN%/*}
+export GIT_REPO=${GIT_ORIGIN#*/}
+
+export LINUX_SRC=CHIP-linux
+
+export ARCH=arm
+export KBUILD_DEBARCH=armhf
+export CROSS_COMPILE=arm-linux-gnueabihf-
+export KDEB_CHANGELOG_DIST=jessie
+export LOCALVERSION=-${GIT_USER}-${GIT_REPO}
+
+export KDEB_PKGVERSION=$(cd $LINUX_SRC; make kernelversion)-$YOUR_VERSION
+export DEBFULLNAME="$YOUR_FULLNAME"
+export DEBEMAIL="$YOUR_EMAIL"
+
+
+#  make -j4 deb-pkg
