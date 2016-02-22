708a5173932af24c1f454599110bfd4e778d5c3b
Alex Kaplan <kaplan2539@gmail.com>
2016-02-21 18:13:36 -0800

    some cleanup

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-function echo_var() {
-  eval echo "$1=\$$1"
-}
-
 set -x
 
 export BUILD="${TRAVIS_BUILD_NUMBER}"
@@ -11,6 +7,7 @@ export BUILD="${TRAVIS_BUILD_NUMBER}"
 export GIT_ORIGIN=$(git remote -v |head -n1|sed -r -e 's/.*(http(s)?:?\/\/|\w@)[A-Za-z0-9.]*[\/:]([^ ]*).*/\3/g; s/\.git//g')
 export GIT_USER=${GIT_ORIGIN%/*}
 export GIT_REPO=${GIT_ORIGIN#*/}
+
 
 #install package necessary for compilation
 sudo apt-get -y install lzop
@@ -24,6 +21,8 @@ tar xf gcc-linaro-arm-linux-gnueabihf-4.9-2014.09_linux.tar.xz
 #export CC="${PWD}/gcc-linaro-arm-linux-gnueabihf-4.9-2014.09_linux/bin/arm-linux-gnueabihf-"
 #export PATH="${PWD}/gcc-linaro-arm-linux-gnueabihf-4.9-2014.09_linux/bin":"${PWD}/gcc-linaro-arm-linux-gnueabihf-4.9-2014.09_linux/arm-linux-gnueabihf/bin/":"${PATH}"
 export PATH="${PWD}/gcc-linaro-arm-linux-gnueabihf-4.9-2014.09_linux/bin":"${PATH}"
+
+
 
 #checkout Linux
 export LINUX_SRC="${PWD}/CHIP-linux"
@@ -44,3 +43,5 @@ export DEBFULLNAME="${GIT_USER}"
 export DEBEMAIL="${GIT_USER}@github.com"
 
 make -j8 deb-pkg
+
+ls -ltra
