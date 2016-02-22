e8a0c0a57f4ce255a4554a45d7f5d8d14b30330a
Alex Kaplan <kaplan2539@gmail.com>
2016-02-21 17:11:03 -0800

    debugging build.sh 2

--- build.sh
+++ build.sh
@@ -20,8 +20,10 @@ tar xf gcc-linaro-arm-linux-gnueabihf-4.9-2014.09_linux.tar.xz
 export PATH="${PWD}/gcc-linaro-arm-linux-gnueabihf-4.9-2014.09_linux/bin":"${PWD}/gcc-linaro-arm-linux-gnueabihf-4.9-2014.09_linux/arm-linux-gnueabihf/bin/":"${PATH}"
 which arm-linux-gnueabihf-gcc
 arm-linux-gnueabihf-gcc --version
+arm-linux-gnueabihf-gcc-4.9.2 --version
 
-ls -l ${PWD}/gcc-linaro-arm-linux-gnueabihf-4.9-2014.09_linux/bin/
+ls -l ${PWD}/gcc-linaro-arm-linux-gnueabihf-4.9-2014.09_linux/bin/arm-linux-gnueabihf-gcc
+ls -l ${PWD}/gcc-linaro-arm-linux-gnueabihf-4.9-2014.09_linux/bin/arm-linux-gnueabihf-gcc-4.9.2
 exit 1
 
 #checkout Linux
